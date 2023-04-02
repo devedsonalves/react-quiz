5f35a3b433395b7eb142fb9f1c3c7ea3c44216f1
devedsonalves <devedsonalves@gmail.com>
2023-04-02 18:42:48 -0300

    styled with sucess 2.0

--- client/src/data/perguntas.js
+++ client/src/data/perguntas.js
@@ -2,56 +2,37 @@ export const Perguntas = [
   {
     pergunta: "O que é ciência?",
     opcoesResposta: [
-      { resposta: "ERRADA", correta: false, alternativa: "A)" },
-      { resposta: "ERRADA", correta: false, alternativa: "B)" },
-      { resposta: "ERRADA", correta: false, alternativa: "C)" },
-      {
-        resposta:
-          "Ciência é um esforço para entender como o universo funciona, feito através de estudos e pesquisas que seguem um método chamado científico",
-        correta: true,
-        alternativa: "D)",
-      },
+      { resposta: "...", correta: true, alternativa: "A)" },
+      { resposta: "...", correta: false, alternativa: "B)" },
+      { resposta: "...", correta: false, alternativa: "C)" },
+      { resposta: "...", correta: false, alternativa: "D)" },
     ],
   },
   {
     pergunta: "O que são ciências exatas?",
     opcoesResposta: [
-      { resposta: "ERRADA", correta: false, alternativa: "A)" },
-      { resposta: "ERRADA", correta: false, alternativa: "B)" },
-      {
-        resposta:
-          "São aquelas em que os resultados são precisos e altamente preditivos, como matemática, física e química.",
-        correta: true,
-        alternativa: "C)",
-      },
-      { resposta: "ERRADA", correta: false, alternativa: "D)" },
+      { resposta: "...", correta: true, alternativa: "A)" },
+      { resposta: "...", correta: false, alternativa: "B)" },
+      { resposta: "...", correta: false, alternativa: "C)" },
+      { resposta: "...", correta: false, alternativa: "D)" },
     ],
   },
   {
     pergunta: "O que são ciências inexatas?",
     opcoesResposta: [
-      { resposta: "ERRADA", correta: false, alternativa: "A)" },
-      { resposta: "ERRADA", correta: false, alternativa: "B)" },
-      { resposta: "ERRADA", correta: false, alternativa: "C)" },
-      {
-        resposta:
-          "São aquelas em que os resultados nem sempre são os esperados, como meteorologia e economia",
-        correta: true,
-        alternativa: "D)",
-      },
+      { resposta: "...", correta: true, alternativa: "A)" },
+      { resposta: "...", correta: false, alternativa: "B)" },
+      { resposta: "...", correta: false, alternativa: "C)" },
+      { resposta: "...", correta: false, alternativa: "D)" },
     ],
   },
   {
     pergunta: "Quais das afirmações diz a respeito das ciências formais?",
     opcoesResposta: [
-      { resposta: "Estudam ideias", correta: true, alternativa: "A)" },
-      { resposta: "ERRADA", correta: false, alternativa: "B)" },
-      { resposta: "ERRADA", correta: false, alternativa: "C)" },
-      {
-        resposta: "ERRADA",
-        correta: false,
-        alternativa: "D)",
-      },
+      { resposta: "...", correta: true, alternativa: "A)" },
+      { resposta: "...", correta: false, alternativa: "B)" },
+      { resposta: "...", correta: false, alternativa: "C)" },
+      { resposta: "...", correta: false, alternativa: "D)" },
     ],
   },
 ];
